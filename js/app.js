10b6e80ca0e8219be90a8e1cb2865636365ff4d1
santiago chinellato <santiagochinellato@gmail.com>
2021-01-06 13:22:17 -0300

    cambios en el index 6/1

--- js/app.js
+++ js/app.js
@@ -51,3 +51,21 @@ var galleryThumbs = new Swiper('.gallery-thumbs',{
       swiper: galleryThumbs
     }
   });
+
+  var galleryThumbs = new Swiper('.gallery-thumbs4',{
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top4',{
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
